a1ff959af6a3d4d4f1a627c7d73d374899de6aca
sinryuji <siler2474@gmail.com>
2024-10-09 18:51:33 +0900

    how to use a function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,23 @@
+fun sum01(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum02(a: Int, b: Int) = a + b
+
+fun printSum01(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
+}
+
+fun printSum02(a: Int, b: Int) {
+    println("sum of $a and $b is ${a + b}")
+}
+
 fun main() {
-    println("Enter any word: ")
-    val yourWord = readln()
-    print("You entered the word: ")
-    print(yourWord)
+    val a: Int = 3
+    val b: Int = 5
+
+    println(sum01(a, b))
+    println(sum02(a, b))
+    printSum01(a, b)
+    printSum02(a, b)
 }
