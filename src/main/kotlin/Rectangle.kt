07373731a85ce6129cc0ea80a7b5e6595e9bcfb9
sinryuji <siler2474@gmail.com>
2024-10-10 18:40:29 +0900

    class

---
+++ src/main/kotlin/Rectangle.kt
@@ -0,0 +1,5 @@
+open class Shape
+
+class Rectangle(val height: Double, val length: Double): Shape() {
+    val perimeter = (height + length) * 2
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,4 @@
 fun main() {
+    val rectangle = Rectangle(5.0, 2.0)
+    println("The perimeter is ${rectangle.perimeter}")
 }
