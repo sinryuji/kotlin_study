288d5ee53a7ab0370e61e9c41d8f1dd9dcf9cf43
sinryuji <siler2474@gmail.com>
2024-10-10 20:39:16 +0900

    while loop

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,3 @@
 fun main() {
-    forloop()
-    forloopIndex()
+    whileLoop()
 }
---
+++ src/main/kotlin/whileLoop.kt
@@ -0,0 +1,8 @@
+fun whileLoop(): Unit {
+    val items = listOf("apple", "banana", "kiwifruit")
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+}
