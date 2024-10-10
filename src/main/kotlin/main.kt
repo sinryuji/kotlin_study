757002a17d6d028b0d82355ec809adc707bb31d7
sinryuji <siler2474@gmail.com>
2024-10-10 20:44:41 +0900

    when expression

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    whileLoop()
+    println(describe(1))
+    println(describe("Hello"))
+    val a: Long = 123
+    println(describe(a))
+    val b: Int = 123
+    println(describe(b))
+    println(describe("hi"))
 }
